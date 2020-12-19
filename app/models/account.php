ff08ff23545cba9e2fa5d9d58c21cbf6d4eec610
unknown <nethsajo98@gmail.com>
2020-12-19 19:11:45 +0800

    Fixed bugs

--- app/models/account.php
+++ app/models/account.php
@@ -3,10 +3,11 @@
 	use PHPMailer\PHPMailer\Exception;
 
 	class account extends Model {
-		public function __construct() {
-			parent::__construct();
-		}
-		
+
+        public function __construct() {
+            parent::__construct();
+        }
+
         public function total_employees($role) {
             $query = $this->db->query("SELECT * FROM tbl_accounts WHERE employee_role_id = $role");
             return $query->num_rows;
@@ -22,37 +23,39 @@
             return $query;
         }
 		
-		public function get_user_information($employee_id) {
-			$query = $this->db->query("SELECT * FROM tbl_accounts AS ta INNER JOIN tbl_roles AS tr ON ta.employee_role_id = tr.role_id WHERE employee_id = $employee_id");
-			return $query->fetch_object();
-		}
+        public function get_user_information($employee_id) {
+            $query = $this->db->query("SELECT * FROM tbl_accounts AS ta INNER JOIN tbl_roles AS tr ON ta.employee_role_id = tr.role_id WHERE employee_id = $employee_id");
+            return $query->fetch_object();
+        }
 		
-		public function user_login($data) {
-			$account_number = $data['account_number'];
-			$account_password = $data['account_password'];
-			$check = $this->db->query("SELECT * FROM tbl_accounts AS ta INNER JOIN tbl_roles AS tr ON ta.employee_role_id = tr.role_id WHERE ta.employee_number = '$account_number'");
-			if($check->num_rows > 0) {
-				$row = $check->fetch_object();
+        public function user_login($data) {
+            $account_number = $data['account_number'];
+            $account_password = $data['account_password'];
+            $check = $this->db->query("SELECT * FROM tbl_accounts AS ta INNER JOIN tbl_roles AS tr ON ta.employee_role_id = tr.role_id WHERE ta.employee_number = '$account_number'");
+            
+            if($check->num_rows > 0) {
+                $row = $check->fetch_object();
                 $employee_role_id = $row->employee_role_id;
                 $employee_status = $row->employee_status;
-				$employee_hash_password = $row->employee_password;
-				if(verify($account_password, $employee_hash_password) && $employee_role_id == 1 && $employee_status == 'Active') {
-					$_SESSION['account_id'] = $row->employee_id;
-					echo json_encode(['url' => URL.'admin/dashboard','success' => true]);
-				} elseif(verify($account_password, $employee_hash_password) && $employee_role_id == 2 && $employee_status == 'Active') {
-					$_SESSION['account_id'] = $row->employee_id;
-					echo json_encode(['url' => URL.'employee/index','success' => true]);
+                $employee_hash_password = $row->employee_password;
+                
+                if(verify($account_password, $employee_hash_password) && $employee_role_id == 1 && $employee_status == 'Active') {
+                    $_SESSION['account_id'] = $row->employee_id;
+                    echo json_encode(['url' => URL.'admin/dashboard','success' => true]);
+                } elseif(verify($account_password, $employee_hash_password) && $employee_role_id == 2 && $employee_status == 'Active') {
+                    $_SESSION['account_id'] = $row->employee_id;
+                    echo json_encode(['url' => URL.'employee/index','success' => true]);
                 } elseif(verify($account_password, $employee_hash_password) && $employee_status == 'Not Active') {
                     $message = 'Your account is not active yet. Contact the admin to activate your account.';
                     notify('error', $message, false);
                 } else {
-					$message = 'Your employee number or password was entered incorrectly.';
+                    $message = 'Your employee number or password was entered incorrectly.';
                     notify('error', $message, false);
-				}
-			} else {
-				$message = 'Your employee number or password was entered incorrectly.';
+                }
+            } else {
+                $message = 'Your employee number or password was entered incorrectly.';
                 notify('error', $message, false);
-			}
+            }
         }
         
         public function update_password($data) {
@@ -82,9 +85,9 @@
             }
         }
 
-		public function user_forgot_password($forgot_password_email) {
-			$check = $this->db->query("SELECT * FROM tbl_accounts WHERE employee_email = '$forgot_password_email'");
-			if($check->num_rows > 0) {
+        public function user_forgot_password($forgot_password_email) {
+            $check = $this->db->query("SELECT * FROM tbl_accounts WHERE employee_email = '$forgot_password_email'");
+            if($check->num_rows > 0) {
                 $row = $check->fetch_object();
                 $employee_id   = $row->employee_id;
                 $employee_name = $row->employee_firstname . ' ' . $row->employee_surname;
@@ -105,7 +108,7 @@
                         $SMTP_password	= $SMTP_row->smtp_password;
                         $SMTP_from		= $SMTP_row->smtp_from;
                         $_SESSION['security_code'] = $security_code;
-                        
+
                         $mail = new PHPMailer();
                         $mail->isSMTP();
                         $mail->Host = $SMTP_socket;
@@ -120,8 +123,7 @@
                         $mail->setFrom("no-reply@aczadmin.com", $SMTP_from);
                         $mail->addAddress($forgot_password_email);
                         $mail->Subject = "Recover Your Account";
-                        $mail->Body = 
-                        '
+                        $mail->Body = '
                         <div style="width:500px;background:#fff;margin:auto;padding:10px">
 
                         <h3 style="font-weight:700;font-size:24px;color:#111;">Reset your password</h3>
@@ -134,8 +136,8 @@
                         <div style="clear:both"></div>
 
                         <h4 style="text-align:justify;font-weight:500;">Security Code : '.$_SESSION['security_code'].'</h4>
-					    <div style="clear:both"></div>
-                        
+                        <div style="clear:both"></div>
+
                         <a href="http://localhost/acz-thesis/login/validate" style="display:inline-block;margin-top:10px;color:#fff;text-decoration:none;cursor:pointer;padding:10px 15px;background-color:#7CB342;font-weight:600;border-radius:5px;">
                             Reset your password
                         </a>';
@@ -156,13 +158,13 @@
                 $message = 'Email not found!';
                 notify('error', $message, false);
             }
-		}
+        }
 
-		public function security_code($data) {
-			$security_code 		   = $data['security_code'];
-			$correct_security_code = $data['correct_security_code'];
-			
-			if($security_code == $correct_security_code) {
+        public function security_code($data) {
+            $security_code 		   = $data['security_code'];
+            $correct_security_code = $data['correct_security_code'];
+
+            if($security_code == $correct_security_code) {
                 unset($_SESSION['security_code']);
                 $_SESSION['correct_security_code'] = $security_code;
                 echo json_encode(['url' => URL.'login/reset','success' => true]);
@@ -170,28 +172,28 @@
                 $message = 'Invalid Security Code!';
                 notify('error', $message, false);
             }
-		}
+        }
 
-		public function reset_old_password($data) {
-			$password_hash			= $data['new_password'];
-			$security_code 		   	= $data['security_code'];
-			$correct_security_code 	= $data['correct_security_code'];
+        public function reset_old_password($data) {
+            $password_hash			= $data['new_password'];
+            $security_code 		   	= $data['security_code'];
+            $correct_security_code 	= $data['correct_security_code'];
 
-			$query 	= $this->db->query("SELECT * FROM tbl_accounts WHERE employee_security_code = '$correct_security_code'");
+            $query 	= $this->db->query("SELECT * FROM tbl_accounts WHERE employee_security_code = '$correct_security_code'");
             $row = $query->fetch_object();
-			$employee_id = $row->employee_id;
-			
-			$query = $this->db->query("UPDATE tbl_accounts SET employee_password = '$password_hash', employee_security_code = '$security_code' WHERE employee_id = $employee_id");
-			
-			unset($_SESSION['correct_security_code']);
+            $employee_id = $row->employee_id;
+
+            $query = $this->db->query("UPDATE tbl_accounts SET employee_password = '$password_hash', employee_security_code = '$security_code' WHERE employee_id = $employee_id");
+
+            unset($_SESSION['correct_security_code']);
 
             if($query) {
-				$content = 'reset his / her password';
+                $content = 'reset his / her password';
                 $this->db->query("INSERT INTO tbl_logs (logs_content, employee_id) VALUES ('$content', $employee_id)");
-			}
-			
-			$_SESSION['message'] = 'Your account is now recovered!';
-			echo json_encode(['url' => URL.'login','success' => true]);
+            }
+
+            $_SESSION['message'] = 'Your account is now recovered!';
+            echo json_encode(['url' => URL.'login','success' => true]);
         }
         
         public function generate_employee_number() {
@@ -246,13 +248,13 @@
 
             $query = $this->db->query("UPDATE tbl_accounts SET employee_status = '$employee_status' WHERE employee_id = $employee_status_id");
             $message = "Status has been updated!";
-            notify('success', $message, true);
+            $query ? notify('success', $message, true) : null;
         }
 
         public function delete_employees_by_id($employee_delete_id) {
             $query = $this->db->query("DELETE FROM tbl_accounts WHERE employee_id = $employee_delete_id");
             $message = "Employee has been deleted.";
-            notify('info', $message, true);
+            $query ? notify('info', $message, true) : null;
         }
 
         public function get_all_employees($employee_role_id) {
@@ -300,7 +302,7 @@
         public function delete_banks_by_id($bank_delete_id) {
             $query = $this->db->query("DELETE FROM tbl_banks WHERE bank_id = $bank_delete_id");
             $message = "Bank has been deleted.";
-            notify('info', $message, true);
+            $query ? notify('info', $message, true) : null;
         }
         
         //Expense Category
@@ -338,7 +340,7 @@
         public function delete_expense_category_by_id($expense_category_delete_id) {
             $query = $this->db->query("DELETE FROM tbl_expense_category WHERE category_id = $expense_category_delete_id");
             $message = "Category has been deleted.";
-            notify('info', $message, true);
+            $query ? notify('info', $message, true) : null;
         }
 
         //Expense Category
@@ -379,13 +381,13 @@
             $query ? notify('info', $message, true) : null;
         }
 
-		public function post($data) {
-			$query = $this->db->real_escape_string(htmlentities($_POST[$data]));
-			return $query;
-		}
-		
-		public function get($data) {
-            $query = $this->db->real_escape_string(htmlentities($_GET[$data]));
-			return $query;
+        public function post($data) {
+            $query = $this->db->real_escape_string(htmlentities($_POST[$data]));
+            return $query;
         }
-	}
+		
+        public function get($data) {
+            $query = $this->db->real_escape_string(htmlentities($_GET[$data]));
+            return $query;
+        }
+    }
